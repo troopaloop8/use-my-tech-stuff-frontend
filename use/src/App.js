eb09bca03a1e92864e452183e0d0d007d8c1a741
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-06-25 00:04:48 -0400

    added semi functional search bar

--- use/src/App.js
+++ use/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import NavBar from './NavBar';
 import SignUp from './SignUp';
@@ -7,8 +7,6 @@ import RenterForm from './RenterForm';
 import TechList from "./TechList";
 import Product from "./Product"
 import './App.css';
-
-
 function App() {
   const[data,setData] = useState(
     [
@@ -26,7 +24,7 @@ function App() {
         {
             "prod_id": 2,
             "name": "iPhone 6s",
-            "image_URL": "https://en.wikipedia.org/wiki/IPhone_6S#/media/File:IPhone_6s_vector.svg",
+            "image_URL": "https://images-na.ssl-images-amazon.com/images/I/61%2BfbdrjtCL._AC_SL1500_.jpg",
             "price": "$6/month",
             "content": "Working condition rose gold iPhone 6s, 64GB storage, unlocked, some battery degredation",
             "ownerName": "poweruser",
@@ -37,7 +35,7 @@ function App() {
         {
             "prod_id": 3,
             "name": "iPhone 7s",
-            "image_URL": "https://en.wikipedia.org/wiki/IPhone_7#/media/File:IPhone_7_Jet_Black.svg",
+            "image_URL": "https://images-na.ssl-images-amazon.com/images/I/61udz3%2B61LL._AC_SL1200_.jpg",
             "price": "$7/month",
             "content": "Mint condition jet black iPhone 7s, 64GB storage, unlocked, dual sim",
             "ownerName": "Jon",
@@ -58,10 +56,10 @@ function App() {
         },
         {
             "prod_id": 6,
-            "name": "testpost UPDATE TEST",
-            "image_URL": "https://www.lotus-qa.com/wp-content/uploads/2020/02/testing.jpg",
+            "name": "iPad",
+            "image_URL": "https://images-na.ssl-images-amazon.com/images/I/61hjzGugXhL._AC_SL1500_.jpg",
             "price": "free",
-            "content": "its nothing, really",
+            "content": "It's just an ipad",
             "ownerName": "Chanis",
             "owner_ID": 5,
             "borrowerName": null,
@@ -69,38 +67,41 @@ function App() {
         }
     ]
 )
+const [searchTerm, setSearchTerm] = useState("")
+const handleChange = e => {
+  setSearchTerm(e.target.value)
+  console.log(searchTerm)
+}
+useEffect(() => {
+  setData(data);
+  const initialArray = data;
+  const filteredData = initialArray.filter((item) => {
+    return item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+    setData(filteredData);
+}, [searchTerm])
   return (
-
     <div className="App">
+      <label>Search for Product Here: <input style={{color: "green"}} type="text" name="search" value={searchTerm} onChange={handleChange} /> </label>
       <header className="Container">
-
         <NavBar/>
-
         <Route exact path = '/'>
-          
           <TechList products = {data}/>
-
         </Route>
-
         <Route path = '/sign-in'>
           <SignIn/>
         </Route>
-
         <Route path = '/sign-up'>
           <SignUp/>
         </Route>
-
         <Route path = '/renter-form'>
           <RenterForm/>
         </Route>
-        
-        <Route path = '/product/:productID'>
+        <Route path = '/product/:itemId'>
           <Product product = {data}/>
         </Route>
-
       </header>
     </div>
-
   );
 }
 export default App;
